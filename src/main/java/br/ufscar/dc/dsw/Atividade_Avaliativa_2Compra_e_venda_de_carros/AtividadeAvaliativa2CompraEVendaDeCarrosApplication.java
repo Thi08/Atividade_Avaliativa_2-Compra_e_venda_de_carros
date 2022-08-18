12c3a8d5c8502d48fab5d584931994d4656eeefc
Thiago Albino <thiagoalbino2000@hotmail.com>
2022-08-18 13:23:18 -0300

    Enviando pastas

---
+++ src/main/java/br/ufscar/dc/dsw/Atividade_Avaliativa_2Compra_e_venda_de_carros/AtividadeAvaliativa2CompraEVendaDeCarrosApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.Atividade_Avaliativa_2Compra_e_venda_de_carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeAvaliativa2CompraEVendaDeCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeAvaliativa2CompraEVendaDeCarrosApplication.class, args);
+	}
+
+}
