a3a1c3414fd72962d390863bcb06075819fd0c70
Bruno <brunoleandropereira@hotmail.com>
2022-08-22 10:47:50 -0300

    Página Home

--- src/main/java/br/ufscar/dc/dsw/Atividade_Avaliativa_2Compra_e_venda_de_carros/controller/IndexController.java
+++ src/main/java/br/ufscar/dc/dsw/Atividade_Avaliativa_2Compra_e_venda_de_carros/controller/IndexController.java
@@ -1,19 +1,26 @@
 package br.ufscar.dc.dsw.Atividade_Avaliativa_2Compra_e_venda_de_carros.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.ufscar.dc.dsw.Atividade_Avaliativa_2Compra_e_venda_de_carros.dao.IUserDAO;
+import br.ufscar.dc.dsw.Atividade_Avaliativa_2Compra_e_venda_de_carros.dao.IVeiculoDAO;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private IVeiculoDAO iVeiculoDAO;
+
+    @Autowired
+    private IUserDAO iUserDAO;
+
     @GetMapping("/")
-    public String index(Model model) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss - dd MMMM yyyy");
-        Calendar cal = Calendar.getInstance();
-        model.addAttribute("date", dateFormat.format(cal.getTime()));
-        return "index";
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("/index");
+        mv.addObject("listaVeiculos", iVeiculoDAO.findAll());
+        return mv;
     }
 }
